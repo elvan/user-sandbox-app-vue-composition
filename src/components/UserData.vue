9372b31453dfda1ddcf338a0992c1defa59d178d
Elvan Hidayat <elvanhd@gmail.com>
2024-03-25 22:25:11 +0700

    Enhance UserData with Vue lifecycle hooks
    - Incorporate multiple Vue lifecycle hooks in UserData.vue, including onBeforeMount, onMounted, onBeforeUpdate, onUpdated, onBeforeUnmount, and onUnmounted
    - Use these hooks to log lifecycle events, aiding in debugging and understanding component behavior
    - Maintain use of computed and inject APIs for reactive userName composition and age injection
    - Demonstrate practical application of Vue's Composition API lifecycle hooks for component lifecycle management

--- src/components/UserData.vue
+++ src/components/UserData.vue
@@ -6,7 +6,16 @@
 </template>
 
 <script>
-import { computed, inject } from 'vue';
+import {
+  computed,
+  inject,
+  onBeforeMount,
+  onBeforeUnmount,
+  onBeforeUpdate,
+  onMounted,
+  onUnmounted,
+  onUpdated,
+} from 'vue';
 
 export default {
   props: ['firstName', 'lastName'],
@@ -18,6 +27,30 @@ export default {
     const age = inject('userAge');
 
     console.log(context);
+
+    onBeforeMount(function () {
+      console.log('onBeforeMount');
+    });
+
+    onMounted(function () {
+      console.log('onMounted');
+    });
+
+    onBeforeUpdate(function () {
+      console.log('onBeforeUpdate');
+    });
+
+    onUpdated(function () {
+      console.log('onUpdated');
+    });
+
+    onBeforeUnmount(function () {
+      console.log('onBeforeUnmount');
+    });
+
+    onUnmounted(function () {
+      console.log('onUnmounted');
+    });
 
     return { userName: uName, age };
   },
